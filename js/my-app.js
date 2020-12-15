cdf70a774806067806011d8fb19dcbca398181e4
pavon-97 <70551412+pavon-97@users.noreply.github.com>
2020-12-14 21:56:49 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,76 @@
+// Initialize your app
+var app = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	panel:{
+		swipe:true
+	},
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'CoffeeApp',
+	dialog: {
+		title: 'CoffeeApp',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		},
+		{
+		path: '/info/',
+    	url: 'info.html',
+    	name: 'info',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+$$('#btnComprar').on('click', function () {
+  app.dialog.alert('¡ Tu compra fue realizada correctamente !');
+});
+
+
+$$('#btnConfirmar').on('click', function () {
+  app.dialog.confirm('¿Seguro desea procesar su compra?', function () {
+    app.dialog.alert('¡ Tu compra fue realizada correctamente !');
+  });
+});
+
+
+$$('#btnLogin').on('click', function () {
+	var Usuario = $$('#username').val();
+	var Password = $$('#password').val();
+	
+  	if(Usuario == "jframos" && Password == "123"){
+		app.dialog.alert('¡ Bienvenido a Coffee Shop !');
+		app.loginScreen.close(".modal-login");
+		
+	}else{
+		app.dialog.alert('Lo sentimos, sus datos son incorrectos');
+	}
+	
+	
+});
+
+
+
+
+
+
+
+ 
+
